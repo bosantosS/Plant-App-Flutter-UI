2ad2e2e133fa233e9df69917c3831f6158072cda
bosantosS <bosantossantistevan@gmail.com>
2021-06-25 08:29:42 -0500

    Faul: Test in real devices and complete de CRUD operations

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,8 +1,18 @@
 import 'package:flutter/material.dart';
 
 // Colors that we use in our app
-const kPrimaryColor = Color(0xFF0C9869);
-const kTextColor = Color(0xFF3C4046);
-const kBackgroundColor = Color(0xFFF9F8FD);
+const kPrimaryColor = Color(0xFF404fa8);
+const kTextColor = Color(0xFFa2d5f2);
+const kBackgroundColor = Color(0xFFefcf02);
 
 const double kDefaultPadding = 20.0;
+/*
+COLORS
+efcf02
+404fa8
+40a8c4
+a2d5f2
+
+ffc93c
+07689f
+*/
